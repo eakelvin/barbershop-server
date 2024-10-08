ae4630e96b9ab27725cabcb43461bdb664b6ae93
Kelvin Akaba <akabakelvin@gmail.com>
2024-10-08 20:58:49 +0000

    init index

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv/config');
+const express = require('express');
+const cors = require('cors');
+const PORT = process.env.PORT;
+const server = express();
+const api = process.env.URL;
+
+server.use(cors());
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+server.get('/', (req, res) => {
+    res.send('BARBERSHOP SERVER')
+});
+
+server.listen(
+    PORT, () => console.log(`Server Live On Port ${PORT}`)
+);
